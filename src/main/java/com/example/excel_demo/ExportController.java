29edededc08d1225e086dc524c467b783861a1c8
k1le0 <k1le0@live.com>
2020-08-26 09:11:32 +0800

    second commit

--- src/main/java/com/example/excel_demo/ExportController.java
+++ src/main/java/com/example/excel_demo/ExportController.java
@@ -18,10 +18,10 @@ public class ExportController {
         data.setUsername("wang");
         data.setPassword("150361");
         lists.add(data);
-        UserData data1 = new UserData();
-        data1.setUsername("xiao");
-        data1.setPassword("111111");
-        lists.add(data1);
+        UserData data2 = new UserData();
+        data2.setUsername("xiao");
+        data2.setPassword("111111");
+        lists.add(data2);
         EasyExcel.write(fileName, UserData.class).sheet("模板").doWrite(lists);
     }
 }
